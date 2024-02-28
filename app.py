8dc4449a1ecd9272e26a42308671c5e2a2bba5d7
felicityfan13 <felicityfan@outlook.com>
2024-02-27 18:11:00 -0800

    add comment as guest

--- app.py
+++ app.py
@@ -1,38 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-# Dummy data for movies and reviews
-movies = {
-    1: {"title": "The Shawshank Redemption", "year": 1994},
-    2: {"title": "The Godfather", "year": 1972},
-    3: {"title": "The Dark Knight", "year": 2008}
-}
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['movie_reviews']
 
-reviews = {
-    1: ["Amazing movie!", "One of the best movies I've ever seen."],
-    2: ["Classic!", "A must-watch for everyone."],
-    3: ["Incredible performance by Heath Ledger."]
-}
+# Create collections
+movies_collection = db['movies']
+comments_collection = db['comments']
+
+# Check if the movies collection is empty
+if movies_collection.count_documents({}) == 0:
+    # If empty, insert the dummy movie data into the movies collection
+    movies_data = [
+        {"_id": 1, "title": "The Shawshank Redemption", "year": 1994},
+        {"_id": 2, "title": "The Godfather", "year": 1972},
+        {"_id": 3, "title": "The Dark Knight", "year": 2008}
+    ]
+    movies_collection.insert_many(movies_data)
 
 @app.route('/')
 def index():
+    # Retrieve all movies from the database
+    movies = list(movies_collection.find())
     return render_template('index.html', movies=movies)
+
+@app.route('/profile')
+def profile():
+    return render_template('login.html')
 
 @app.route('/movie/<int:movie_id>')
 def movie(movie_id):
-    movie_info = movies.get(movie_id)
-    movie_reviews = reviews.get(movie_id, [])
-    return render_template('movie.html', movie=movie_info, reviews=movie_reviews)
+    movie_info = movies_collection.find_one({"_id": movie_id})
+    comments = comments_collection.find({"movie_id": movie_id})
+    return render_template('movie.html', movie=movie_info, comments=comments)
 
-@app.route('/post_review/<int:movie_id>', methods=['POST'])
-def post_review(movie_id):
-    review = request.form['review']
-    if review:
-        if movie_id in reviews:
-            reviews[movie_id].append(review)
-        else:
-            reviews[movie_id] = [review]
+@app.route('/post_comment/<int:movie_id>', methods=['POST'])
+def post_comment(movie_id):
+    print("success or not_______________________")
+    comment = request.form['comment']
+    if comment:
+        comments_collection.insert_one({"movie_id": movie_id, "comment": comment})
     return redirect(url_for('movie', movie_id=movie_id))
 
 if __name__ == '__main__':
