af2db1020816ac57b7e04ef96798926155777dd6
felicityfan13 <felicityfan@outlook.com>
2024-03-04 19:00:49 -0800

    use objectId for comment instead of create new ID manually

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson import ObjectId
 
 app = Flask(__name__)
 
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
-
-# Temporary for test 
-client.drop_database('movie_reviews')
 db = client['movie_reviews']
 
 # Create collections
@@ -16,8 +14,6 @@ comments_collection = db['comments']
 users_collection = db['users']
 movies_comments = db['movies_comments']
 users_comments = db['users_comments']
-
-comment_id = 0
 
 if movies_collection.count_documents({}) == 0:
     # If empty, insert the dummy movie data into the movies collection
@@ -68,30 +64,29 @@ def movie(movie_id):
 
 @app.route('/post_comment/<int:movie_id>', methods=['POST'])
 def post_comment(movie_id):
-    global comment_id
     comment = request.form['comment']
     if comment:
-        comments_collection.insert_one({"_id": comment_id, "comment": comment})
+        result = comments_collection.insert_one({"comment": comment})
+        comment_id = result.inserted_id
         movies_comments.insert_one({"movie_id": movie_id, "comment_id": comment_id})
 
         # update in the future: Retrieve the user id that post each comment
         users_comments.insert_one({"user_id": 1, "comment_id": comment_id})
-    comment_id = comment_id + 1
     return redirect(url_for('movie', movie_id=movie_id))
 
-@app.route('/delete_comment/<int:comment_id>', methods=['POST'])
+@app.route('/delete_comment/<string:comment_id>', methods=['POST'])
 def delete_comment(comment_id):
+    comment_id = ObjectId(comment_id)
     comments_collection.delete_one({"_id": comment_id})
     users_collection.delete_one({"comment_id": comment_id})
     movies_comments.delete_one({"comment_id": comment_id})
     return redirect(url_for('profile'))
 
-@app.route('/edit_comment/<int:comment_id>', methods=['POST'])
+@app.route('/edit_comment/<string:comment_id>', methods=['POST'])
 def edit_comment(comment_id):
+    comment_id = ObjectId(comment_id)
     edited_comment = request.form['edited_comment']
-    print(edit_comment)
     comments_collection.update_one({"_id": comment_id}, {"$set": {"comment": edited_comment}})
-    
     return redirect(url_for('profile'))
 
 if __name__ == '__main__':
